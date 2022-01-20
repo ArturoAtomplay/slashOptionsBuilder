173f815739626059fd25ccdddcffeaf469b99748
arturoAtomplay <motagratis6@gmail.com>
2022-01-19 21:36:48 -0600

    redoing these classes

--- src/class/CHAT_INPUT.ts
+++ src/class/CHAT_INPUT.ts
@@ -1,148 +1,164 @@
-import { ChatInputApplicationCommandData, ApplicationCommandOptionData, BaseApplicationCommandOptionsData } from "discord.js";
-import { ApplicationCommandChannelOption, ApplicationCommandChoicesOption } from "../types";
-import SubCommand from "./command/SubCommand";
-import SubCommandGroup from "./command/SubCommandGroup";
+import { ApplicationCommandOptionData, ChatInputApplicationCommandData, ExcludeEnum } from "discord.js";
+import { ChannelTypes } from "discord.js/typings/enums";
+import { BaseCommandOptionsData, CommandChoicesData, numberAndChoicesOptions } from "../types";
 
-export default class CHAT_INPUT {
+class Chat_Input {
   private name!: string;
   private description!: string;
-  private defaultPermission!: boolean;
+  private defaultPermission: boolean = true;
   private options: ApplicationCommandOptionData[] = [];
 
-  private readonly type = "CHAT_INPUT";
-
-  setName(name: string) {
+  setName(name: string): this {
+    if (!name) throw new Error("Name cannot be empty");
     if (typeof name !== "string") throw new Error("Name must be a string");
+
     this.name = name;
     return this;
   }
 
-  setDescription(description: string) {
+  setDescription(description: string): this {
+    if (!description) throw new Error("Description cannot be empty");
     if (typeof description !== "string") throw new Error("Description must be a string");
+
     this.description = description;
     return this;
   }
 
-  setDefaultPermission(defaultPermission: boolean) {
-    if (typeof defaultPermission !== "boolean") throw new Error("Default Permission must be a boolean");
-    this.defaultPermission = defaultPermission;
+  setDefaultPermission(permission: boolean): this {
+    if (typeof permission !== "boolean") throw new Error("Permission must be a boolean");
+
+    this.defaultPermission = permission;
     return this;
   }
 
-  addBooleanOption(booleanOptions: BaseApplicationCommandOptionsData) {
-    const { name, description, required, autocomplete } = booleanOptions;
-    const data: ApplicationCommandOptionData = { name, description, type: "BOOLEAN" };
+  addBooleanOption(booleanOptions: BaseCommandOptionsData): this {
+    const { name, description, required = false } = booleanOptions;
 
-    if (required) data.required = true;
-    if (autocomplete) data.autocomplete = true;
+    if (!name) throw new Error("Name is required");
+    if (typeof name !== "string") throw new Error("Name must be a string");
+
+    if (!description) throw new Error("Description is required");
+    if (typeof description !== "string") throw new Error("Description must be a string");
+
+    if (required && typeof required !== "boolean") throw new Error("Required must be a boolean");
+
+    this.options.push({ name, description, type: "BOOLEAN", required });
+
+    return this;
+  }
+
+  addChannelOption(channelOptions: {
+    name: string;
+    description: string;
+    channelTypes?: ExcludeEnum<typeof ChannelTypes, "UNKNOWN">[];
+    required?: boolean;
+  }): this {
+    const { name, description, channelTypes, required = false } = channelOptions;
+
+    if (!name) throw new Error("Name is required");
+    if (typeof name !== "string") throw new Error("Name must be a string");
+
+    if (!description) throw new Error("Description is required");
+    if (typeof description !== "string") throw new Error("Description must be a string");
+    if (channelTypes && !Array.isArray(channelTypes)) throw new Error("ChannelTypes must be an array");
+    if (typeof required !== "boolean") throw new Error("Required must be a boolean");
+
+    this.options.push({ name, description, type: "CHANNEL", channelTypes, required });
+
+    return this;
+  }
+
+  addIntegerOption(integerOptions: numberAndChoicesOptions): this {
+    const { name, description, required, autocomplete } = integerOptions;
+    const data: any = { name, description, type: "INTEGER", required };
+    if (!autocomplete) {
+      const { choices, minValue, maxValue } = integerOptions;
+      if (choices) data.choices = choices;
+      if (minValue) data.minValue = minValue;
+      if (maxValue) data.maxValue = maxValue;
+    } else if (autocomplete === true) data.autocomplete = true;
 
     this.options.push(data);
     return this;
   }
 
-  addChannelOption(channelOptions: ApplicationCommandChannelOption) {
-    const { name, description, channelTypes, required, autocomplete } = channelOptions;
-    const data: ApplicationCommandOptionData = { name, description, type: "CHANNEL" };
+  addMentionableOption(mentionableOptions: BaseCommandOptionsData): this {
+    const { name, description, required = false } = mentionableOptions;
 
-    if (required) data.required = true;
-    if (autocomplete) data.autocomplete = true;
-    if (channelTypes && channelTypes.length > 0) data.channelTypes = channelTypes;
+    if (!name) throw new Error("Name is required");
+    if (typeof name !== "string") throw new Error("Name must be a string");
+
+    if (!description) throw new Error("Description is required");
+    if (typeof description !== "string") throw new Error("Description must be a string");
+
+    if (required && typeof required !== "boolean") throw new Error("Required must be a boolean");
+
+    this.options.push({ name, description, type: "MENTIONABLE", required });
+
+    return this;
+  }
+
+  addNumberOption(numberOption: numberAndChoicesOptions): this {
+    const { name, description, required, autocomplete } = numberOption;
+    const data: any = { name, description, type: "NUMBER", required };
+    if (!autocomplete) {
+      const { choices, minValue, maxValue } = numberOption;
+      if (choices) data.choices = choices;
+      if (minValue) data.minValue = minValue;
+      if (maxValue) data.maxValue = maxValue;
+    } else if (autocomplete === true) data.autocomplete = true;
 
     this.options.push(data);
     return this;
   }
 
-  addIntegerOption(integerOptions: ApplicationCommandChoicesOption) {
-    const { name, description, choices, required, autocomplete } = integerOptions;
-    const data: ApplicationCommandOptionData = { name, description, type: "INTEGER" };
+  addRoleOption(roleOption: { name: string; description: string; required?: boolean }): this {
+    const { name, description, required = false } = roleOption;
 
-    if (required) data.required = true;
-    if (autocomplete) data.autocomplete = true;
-    if (choices && choices.length > 0) data.choices = choices;
+    if (!name) throw new Error("Name is required");
+    if (typeof name !== "string") throw new Error("Name must be a string");
+
+    if (!description) throw new Error("Description is required");
+    if (typeof description !== "string") throw new Error("Description must be a string");
+
+    if (required && typeof required !== "boolean") throw new Error("Required must be a boolean");
+
+    this.options.push({ name, description, type: "ROLE", required });
+
+    return this;
+  }
+
+  addStringOption(stringOption: CommandChoicesData): this {
+    const { name, description, required = false, choices } = stringOption;
+
+    if (!name) throw new Error("Name is required");
+    if (typeof name !== "string") throw new Error("Name must be a string");
+
+    if (!description) throw new Error("Description is required");
+    if (typeof description !== "string") throw new Error("Description must be a string");
+
+    if (required && typeof required !== "boolean") throw new Error("Required must be a boolean");
+
+    if (choices && !Array.isArray(choices)) throw new Error("Choices must be an array");
+
+    const data: ApplicationCommandOptionData = { name, description, type: "STRING", required };
+
+    if (choices) data.choices = choices;
 
     this.options.push(data);
+
     return this;
   }
 
-  addMentionableOption(mentionableOptions: BaseApplicationCommandOptionsData) {
-    const { name, description, required, autocomplete } = mentionableOptions;
-    const data: ApplicationCommandOptionData = { name, description, type: "MENTIONABLE" };
-
-    if (required) data.required = true;
-    if (autocomplete) data.autocomplete = true;
-
-    this.options.push(data);
-    return this;
-  }
-
-  addNumberOption(numberOptions: ApplicationCommandChoicesOption) {
-    const { name, description, choices, required, autocomplete } = numberOptions;
-    const data: ApplicationCommandOptionData = { name, description, type: "NUMBER" };
-
-    if (required) data.required = true;
-    if (autocomplete) data.autocomplete = true;
-    if (choices && choices.length > 0) data.choices = choices;
-
-    this.options.push(data);
-    return this;
-  }
-
-  addRoleOption(roleOptions: BaseApplicationCommandOptionsData) {
-    const { name, description, required, autocomplete } = roleOptions;
-    const data: ApplicationCommandOptionData = { name, description, type: "ROLE" };
-
-    if (required) data.required = true;
-    if (autocomplete) data.autocomplete = true;
-
-    this.options.push(data);
-    return this;
-  }
-
-  addStringOption(stringOptions: ApplicationCommandChoicesOption) {
-    const { name, description, choices, required, autocomplete } = stringOptions;
-    const data: ApplicationCommandOptionData = { name, description, type: "STRING" };
-
-    if (required) data.required = true;
-    if (autocomplete) data.autocomplete = true;
-    if (choices && choices.length > 0) data.choices = choices;
-
-    this.options.push(data);
-    return this;
-  }
-
-  addUserOption(userOptions: BaseApplicationCommandOptionsData) {
-    const { name, description, required, autocomplete } = userOptions;
-    const data: ApplicationCommandOptionData = { name, description, type: "USER" };
-
-    if (required) data.required = true;
-    if (autocomplete) data.autocomplete = true;
-
-    this.options.push(data);
-    return this;
-  }
-
-  addSubcommand(subcommand: (sub: SubCommand) => SubCommand) {
-    const sub = subcommand(new SubCommand());
-    this.options.push(sub.toJSON());
-    return this;
-  }
-
-  addSubcommandGroup(commandsGroup: (group: SubCommandGroup) => SubCommandGroup) {
-    const subCommandsGroup = commandsGroup(new SubCommandGroup());
-    this.options.push(subCommandsGroup.toJSON());
-    return this;
-  }
-
-  toJSON() {
-    const data: ChatInputApplicationCommandData = {
+  toJSON(): ChatInputApplicationCommandData {
+    return {
       name: this.name,
       description: this.description,
-      type: this.type,
+      defaultPermission: this.defaultPermission,
+      options: this.options,
+      type: "CHAT_INPUT",
     };
-
-    if (this.options.length > 0) data.options = this.options;
-    if (this.defaultPermission === false) data.defaultPermission = false;
-
-    return data;
   }
 }
+
+export default Chat_Input;
