3e2d6e6d1e74747c3e04851114cf7ac1d1bd8d74
arturoAtomplay <motagratis6@gmail.com>
2022-01-22 18:42:50 -0600

    test changes

--- test/index.js
+++ test/index.js
@@ -1,45 +1,35 @@
 //@ts-check
-require("dotenv").config();
+
+const { Slash_Chat_Input } = require("../lib/index");
+const slashCommands = require("./commands");
+
 const Discord = require("discord.js");
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"] });
-const { SlashOptions } = require("../lib/index");
-
-const cmd1 = SlashOptions("CHAT_INPUT").setName("test").setDescription("test");
-
-const commands = [cmd1];
 
 client.on("ready", () => {
   console.log("I am ready!");
 });
 
-client.on("messageCreate", (message) => {
-  (() => {
-    if (message.content === "ping") {
-      message.reply("pong");
-    } else if (message.content === "!deploy") {
-      if (message.author.id !== "624712119098802198") return message.reply("You are not authorized to use this command!");
-      message.channel.send("Deploying...").then((m) => {
-        message.guild.commands
-          .set(commands.map((c) => c.toJSON()))
-          .then(() => {
-            m.edit("Deployed!");
-          })
-          .catch((e) => {
-            m.edit("Failed to deploy!");
-            console.error(e);
-          });
-      });
-    }
-  })();
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) return;
+  if (message.content.startsWith("!deploy")) {
+    if (message.author.id !== "624712119098802198") return;
+
+    const msg = await message.channel.send("Deploying...");
+
+    // console.log(slashCommands);
+
+    message.guild.commands
+      .set(slashCommands)
+      .then(() => msg.edit("Deployed!"))
+      .catch((e) => msg.edit(e.message));
+  }
 });
 
 client.on("interactionCreate", (interaction) => {
   if (interaction.isCommand()) {
-    console.log({ commandName: interaction.commandName, options: interaction.options.data });
-    if (interaction.commandName === "test") {
-      interaction.reply("test");
-    }
+    interaction.reply("Hello!");
   }
 });
 
-client.login(process.env.DISCORD_TOKEN);
+client.login("");
