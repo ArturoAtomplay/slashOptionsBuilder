22fb0000e00df6949622f9f3ebe446281c548159
arturoAtomplay <motagratis6@gmail.com>
2022-01-22 18:36:59 -0600

    I change the function and now it exports classes

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,5 @@
-import CHAT_INPUT from "./class/CHAT_INPUT";
-import MESSAGE from "./class/MESSAGE";
-import USER from "./class/USER";
+import Slash_Chat_Input from "./class/Chat_Input";
+import Slash_Message from "./class/message";
+import Slash_User from "./class/user";
 
-function SlashOptions(type: "CHAT_INPUT" | 1): CHAT_INPUT;
-function SlashOptions(type: "USER" | 2): USER;
-function SlashOptions(type: "MESSAGE" | 3): MESSAGE;
-function SlashOptions(): null;
-
-function SlashOptions(type?: "CHAT_INPUT" | "USER" | "MESSAGE" | number) {
-  if (type === "CHAT_INPUT") return new CHAT_INPUT();
-  else if (type === "USER" || type === 2) return new USER();
-  else if (type === "MESSAGE" || type === 3) return new MESSAGE();
-  else return null;
-}
-
-export = { SlashOptions };
+export { Slash_Chat_Input, Slash_Message, Slash_User };
