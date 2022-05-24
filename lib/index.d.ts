a2b36530e1d2f4387e6f794dde75edf72334adad
arturoAtomplay <64356325+arturoAtomplay@users.noreply.github.com>
2022-05-24 01:26:50 -0500

    Delete lib directory

--- lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-import Slash_Chat_Input from './class/ChatInput';
-import Slash_Message from './class/Message';
-import Slash_User from './class/User';
-export { Slash_Chat_Input, Slash_Message, Slash_User };
